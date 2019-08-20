e4f2617bb966900843b68104a6c556b7167841e2
vitamin9527 <54069345+vitamin9527@users.noreply.github.com>
2019-08-20 09:39:03 +0800

    Kotlin练习

---
+++ wtm/2019年8月16日/Kotlin/src/Demo.kt
@@ -0,0 +1,15 @@
+//Kotlin 函数和变量的定义
+class  Greeter(val name:String){
+    fun max(a:Int,b:Int){
+        
+        println("$name: 今年",(if(a>b) a else b),"岁")
+    }
+
+    private fun println(s: String, i: Int, s1: String) {
+        println(s+i+s1)
+    }
+}
+fun main(args: Array<String>) {    // 包级可见的函数，接受一个字符串数组作为参数
+    Greeter("Whorld").max(10,12)        // 分号可以省略
+
+}
---
+++ wtm/2019年8月16日/Kotlin/src/Demo01.kt
@@ -0,0 +1,21 @@
+import com.sun.org.apache.bcel.internal.generic.NEW
+import java.io.BufferedReader
+import java.util.*
+
+//Kotlin 中表达式和语句
+class Num() {
+    fun read(reader: BufferedReader) {
+        val number = try {
+            Integer.parseInt(reader.readLine())
+        } catch (e: NumberFormatException) {
+            return
+        }
+        println(number)
+    }
+}
+
+fun main(args: Array<String>) {
+    Num().hashCode()
+}
+
+
