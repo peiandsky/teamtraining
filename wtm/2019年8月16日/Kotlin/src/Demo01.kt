e95f61ffad6102ea481e17afe5c5e7ce3ec44aae
vitamin9527 <54069345+vitamin9527@users.noreply.github.com>
2019-08-20 22:13:01 +0800

    Kotlin练习

--- wtm/2019年8月16日/Kotlin/src/Demo01.kt
+++ wtm/2019年8月16日/Kotlin/src/Demo01.kt
@@ -2,20 +2,33 @@ import com.sun.org.apache.bcel.internal.generic.NEW
 import java.io.BufferedReader
 import java.util.*
 
-//Kotlin 中表达式和语句
+//Kotlin 类名
 class Num() {
-    fun read(reader: BufferedReader) {
-        val number = try {
-            Integer.parseInt(reader.readLine())
-        } catch (e: NumberFormatException) {
-            return
-        }
-        println(number)
+    //fun 方法名（参数a,参数b）：返回值类型{
+    //     return 返回值 }
+    fun sum(a:Int,b:Int):Int{
+        return  a+b
     }
 }
 
+    //开始的main方法入口
 fun main(args: Array<String>) {
-    Num().hashCode()
+    //定义一个变量（他可以被修改） w被赋值为某个方法
+    var w= Num().sum(8,1)
+    //输出一句话不换行
+    print("8+11=")
+    //输出那个变量
+    println(w)
+    //对变量进行再次赋值
+        w=Num().sum(10,15)
+        println("再次赋值的值为"+w)
+
+    //定义一个不能被修改的变量 并给他赋值
+     val z=Num().sum(10,11)
+     println("不能修改的变量他的值是："+z)
+
+     //再次赋值失败
+     //  z=Num().sum(1,2)
 }
 
 
