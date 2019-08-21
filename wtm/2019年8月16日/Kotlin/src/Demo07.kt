79d8ab8bb0a089de7660e8961169a6b1b86fb894
vitamin9527 <54069345+vitamin9527@users.noreply.github.com>
2019-08-21 21:23:21 +0800

    Kotlin练习2019年8月21日21:22:28

--- wtm/2019年8月16日/Kotlin/src/Demo07.kt
+++ wtm/2019年8月16日/Kotlin/src/Demo07.kt
@@ -5,18 +5,21 @@ fun getStringLengths(obj: Any): Int? {
     }
     return null
 }
+
 //或者再简便一下
-fun  getStringLengthss(obj: Any):Int?{
-    if (obj!is String) return  null
+fun getStringLengthss(obj: Any): Int? {
+    if (obj !is String) return null
     return obj.length
 }
+
 //甚至这样
-fun getStringLengthsup(obj: Any):Int?{
-    if (obj is String&&obj.length>0){
-        return  obj.length
+fun getStringLengthsup(obj: Any): Int? {
+    if (obj is String && obj.length > 0) {
+        return obj.length
     }
-    return  null
+    return null
 }
+
 fun main(args: Array<String>) {
     println(getStringLengths(1))
     println(getStringLengths("1"))
@@ -25,15 +28,15 @@ fun main(args: Array<String>) {
     println(getStringLengthsup(333))
     println(getStringLengthsup("333"))
     //循环
-    val items= listOf("apple","banana","kiwi")
+    val items = listOf("apple", "banana", "kiwi")
 //for循环 自定义（代值） 格式   集合
-    for (ite in items){
+    for (ite in items) {
         //类似javafor循环遍历
         println(ite)
     }
     //或者
 //for循环 自定义（索引值） 格式   集合的索引数组
-    for (index in items.indices){
+    for (index in items.indices) {
         println("item at $index   =   ${items[index]}")
     }
 
--- wtm/2019年8月16日/Kotlin/src/Demo08.kt
+++ wtm/2019年8月16日/Kotlin/src/Demo08.kt
@@ -1,10 +1,10 @@
 fun main(args: Array<String>) {
-    val  items= listOf("apple","banana","kiwi")
-    var  index=0
+    val items = listOf("apple", "banana", "kiwi")
+    var index = 0
     //while循环
-    while (index<items.size){
+    while (index < items.size) {
         println("$index    = ${items[index]}")
-        println(""+index+" = "+items[index] )
+        println("" + index + " = " + items[index])
         index++
     }
 
